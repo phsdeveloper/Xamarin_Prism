48b58f77a4acfed486acbed1b85b04fc09873c7d
Paulo Santos <phs.developer@gmail.com>
2021-01-18 18:47:27 -0300

    Criação do Projeto e configuração inicial do Prism no projeto

---
+++ 01-Testes/PrismTestes/PrismTestes/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Xamarin.Forms.Xaml;
+
+namespace PrismTestes
+{
+    public partial class App : PrismApplication
+    {
+        public App():base(null){ }
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+            await NavigationService.NavigateAsync("MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<Views.MainPage>();
+        }
+
+        protected override void OnStart()
+        {
+        }
+        
+        protected override void OnSleep()
+        {
+        }
+        
+        protected override void OnResume()
+        {
+        }
+    }
+}
+//treterte
